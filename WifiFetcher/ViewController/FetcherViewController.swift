acecfd4ed4c2fbd3bea6ebd649932e00bfc9f106
Alevtina Mashtaler <alya.mashtalez@gmail.com>
2024-09-18 22:46:48 +0300

    adding showResultPanelView to vc

--- WifiFetcher/ViewController/FetcherViewController.swift
+++ WifiFetcher/ViewController/FetcherViewController.swift
@@ -8,10 +8,29 @@
 import UIKit
 
 class FetcherViewController: UIViewController {
+    
+    private let showResultsPanel = ShowResultsPanelView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = .black
+        view.addSubview(showResultsPanel)
+        addConstraints()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+            self.showResultsPanel.resultsButton.isEnabled = true
+        }
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            showResultsPanel.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -30),
+            showResultsPanel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 25),
+            showResultsPanel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -25),
+            showResultsPanel.heightAnchor.constraint(equalToConstant: 68)
+            
+        ])
+        
+        
     }
 
 
