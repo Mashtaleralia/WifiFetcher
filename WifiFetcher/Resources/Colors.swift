5033f74c42d78976db30f56472213bcdc8439c8a
Alevtina Mashtaler <alya.mashtalez@gmail.com>
2024-09-18 22:44:54 +0300

    colorUpdate

--- WifiFetcher/Resources/Colors.swift
+++ WifiFetcher/Resources/Colors.swift
@@ -11,7 +11,7 @@ struct Colors {
     static let lightGreen = UIColor(red: 174/255, green: 230/255, blue: 127/255, alpha: 1)
     static let dimmedGreen = UIColor(red: 174/255, green: 230/255, blue: 127/255, alpha: 0.15)
     static let hintBackgroundColor = UIColor(red: 41/255, green: 44/255, blue: 41/255, alpha: 0.6)
-    static let greenTextColor = UIColor(red: 41/255, green: 44/255, blue: 41/255, alpha: 0.6)
+    static let greenTextColor = UIColor(red: 32/255, green: 48/255, blue: 8/255, alpha: 1)
     
     
 }
