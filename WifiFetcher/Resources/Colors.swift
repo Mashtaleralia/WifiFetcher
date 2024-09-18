2a726e91189397d8f5d9830c02d03d153ac46d29
Alevtina Mashtaler <alya.mashtalez@gmail.com>
2024-09-18 20:25:25 +0300

    base colors added

---
+++ WifiFetcher/Resources/Colors.swift
@@ -0,0 +1,17 @@
+//
+//  Colors.swift
+//  WifiFetcher
+//
+//  Created by Admin on 18.09.2024.
+//
+
+import UIKit
+
+struct Colors {
+    static let lightGreen = UIColor(red: 174/255, green: 230/255, blue: 127/255, alpha: 1)
+    static let dimmedGreen = UIColor(red: 174/255, green: 230/255, blue: 127/255, alpha: 0.15)
+    static let hintBackgroundColor = UIColor(red: 41/255, green: 44/255, blue: 41/255, alpha: 0.6)
+    static let greenTextColor = UIColor(red: 41/255, green: 44/255, blue: 41/255, alpha: 0.6)
+    
+    
+}
