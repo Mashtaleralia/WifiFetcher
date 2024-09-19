ca86e409611c0c0d85b1b0b58324cdd36afa3e9e
Alevtina Mashtaler <alya.mashtalez@gmail.com>
2024-09-19 11:30:36 +0300

    ToDo model added

---
+++ WifiFetcher/Model/ToDo.swift
@@ -0,0 +1,15 @@
+//
+//  ToDo.swift
+//  WifiFetcher
+//
+//  Created by Admin on 19.09.2024.
+//
+
+import Foundation
+
+struct ToDo: Codable {
+    var userId: Int
+    var id: Int
+    var title: String
+    var completed: Bool
+}
