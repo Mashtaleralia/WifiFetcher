238bd7910001d708ed0e33168e8b7cd76fee3655
Alevtina Mashtaler <alya.mashtalez@gmail.com>
2024-09-20 19:52:20 +0300

    todos vc added

---
+++ WifiFetcher/ViewController/TodosViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TodosViewController.swift
+//  WifiFetcher
+//
+//  Created by Admin on 20.09.2024.
+//
+
+import UIKit
+
+class TodosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+    }
+  
+
+}
