57a6b80607ea0601a27307fc43809c5b81e0a07e
Alevtina Mashtaler <alya.mashtalez@gmail.com>
2024-09-22 20:30:37 +0300

    colors for wifibutton

--- WifiFetcher/Resources/Colors.swift
+++ WifiFetcher/Resources/Colors.swift
@@ -13,5 +13,13 @@ struct Colors {
     static let hintBackgroundColor = UIColor(red: 41/255, green: 44/255, blue: 41/255, alpha: 0.6)
     static let greenTextColor = UIColor(red: 32/255, green: 48/255, blue: 8/255, alpha: 1)
     
+    static let primaryGreyButton = UIColor(red: 41/255, green: 44/255, blue: 41/255, alpha: 1)
+    static let secondaryGreyButton = UIColor(red: 30/255, green: 32/255, blue: 30/255, alpha: 1)
+    static let tertiaryGreyButton = UIColor(red: 24/255, green: 24/255, blue: 24/255, alpha: 1)
+    static let backgroundGreyButton = UIColor(red: 21/255, green: 21/255, blue: 21/255, alpha: 1)
     
+    static let primaryGreenButton = UIColor(red: 174/255, green: 230/255, blue: 127/255, alpha: 0.15)
+    static let secondaryGreenButton = UIColor(red: 174/255, green: 230/255, blue: 127/255, alpha: 0.1)
+    static let tertiaryGreenButton = UIColor(red: 174/255, green: 230/255, blue: 127/255, alpha: 0.06)
+    static let backgroundGreenButton = UIColor(red: 174/255, green: 230/255, blue: 127/255, alpha: 0.03)
 }
